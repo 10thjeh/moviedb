defc7349330cac97d4783cbe7b8d51842bedf104
10thjeh <glock.evans8@gmail.com>
2020-12-03 13:41:44 +0700

    Random Fetch

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import React from 'react';
 
 let imgBaseURL = 'https://image.tmdb.org/t/p/w500';
 
+function generateRandomID(){
+  let a = Math.floor(Math.random()*100)+500;
+  return a;
+}
+
+let randomID = generateRandomID();
+
 class Fetch extends React.Component{
   constructor(props){
     super(props);
@@ -15,7 +22,7 @@ class Fetch extends React.Component{
   }
 
   componentDidMount(){
-   const urlFetch = fetch('https://api.themoviedb.org/3/movie/550?api_key=ef3b7e9dfef3057a8dba1691a414348c')
+   const urlFetch = fetch('https://api.themoviedb.org/3/movie/'+randomID+'?api_key=ef3b7e9dfef3057a8dba1691a414348c')
    urlFetch.then( res => {
       if(res.status === 200)
          return res.json()
